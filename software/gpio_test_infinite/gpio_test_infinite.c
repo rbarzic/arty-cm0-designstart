78238658952e49f902f69cf02631a91ae3c37814
Ronan Barzic <rbarzic@gmail.com>
2015-12-17 11:32:33 +0100

    Add mmi file that seems to be valid

---
+++ software/gpio_test_infinite/gpio_test_infinite.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "CMSDK_CM0.h"
+
+#define small_delay  __ISB
+
+void medium_delay() {
+    for(uint32_t i=0;i<10;i++) {
+        small_delay();
+    }
+
+}
+
+void long_delay() {
+    for(uint32_t i=0;i<200000;i++) {
+        medium_delay();
+    }
+
+}
+
+
+
+int main (void)
+{
+    CMSDK_GPIO0->OUTENABLESET = 0x00000001;
+    while(1) {
+        // for(int i =0; i<5; i++) {
+        CMSDK_GPIO0-> DATAOUT = 0;
+        long_delay();
+        CMSDK_GPIO0-> DATAOUT = 1;
+        long_delay();
+    }
+    return 0;
+    // exit(0);
+}
